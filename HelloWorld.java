295015d976cb8c3a660d818c113405cb9acc247e
osorcom <oscar.sor.com@gmail.com>
2024-09-18 17:23:02 +0200

    subrallat

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args){
-    System.out.println("Hello World 24-25");
+    System.out.println("Hello World 24-25\n----------------------------------");
   }
 }
