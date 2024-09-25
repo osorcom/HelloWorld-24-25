d1785db8b3ff65ce001158e5bd877c9b676bd15a
EnricCostaSierra <adam23ecosta@inslaferreria.cat>
2024-09-25 16:44:45 +0200

    escriure Enric

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args){
     System.out.println("Hello World 24-25\n----------------------------------");
     System.out.println("osc");
+     System.out.println("Enric Costa");
   }
 }
