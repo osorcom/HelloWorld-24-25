ad9eb38c686889beafa1bd1881444c024b82e7ff
IkerVargas <adam23ivargas@inslaferreria.cat>
2024-09-25 16:58:00 +0200

    nombre iker2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args){
     System.out.println("Hello World 24-25\n----------------------------------");
-<<<<<<< HEAD
     System.out.println("Iker Vargas");
 
   }
