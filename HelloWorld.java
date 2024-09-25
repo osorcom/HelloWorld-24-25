6f09ff5f802e1f4803f9a405c0268d59bbcc1def
LuisTortosa <adam23ltortosa@inslaferreria.cat>
2024-09-25 17:14:03 +0200

    hola caracola

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
+
 public class HelloWorld {
   public static void main(String[] args){
     System.out.println("Hello World 24-25\n----------------------------------");
-<<<<<<< HEAD
-    System.out.println("Luis :,D :) :( :o (º_º) (º\º):D");
-    System.out.println("osc");
-    System.out.println("sebastrg");
-        System.out.println("Hello World 24-25\n----------------------------------");
-=======
+
     System.out.println("Iker Vargas");
 
->>>>>>> 482f665617018aafe98176002538f46b72778d41
         System.out.println("Àlex Hernández Gallego");
 
         System.out.println("Sebastian");
@@ -22,13 +16,11 @@ public class HelloWorld {
         System.out.println("osc");
 
         System.out.println("Hola bebe");
-<<<<<<< HEAD
-	System.out.println("luis tonto");
-=======
+	System.out.println("luis :D  (º_º)");
 
 	Persona p1 = new Persona("AlexLopez", 18, "Masculino");
 	p1.getSexo();
->>>>>>> 482f665617018aafe98176002538f46b72778d41
+
     }
 
 }
