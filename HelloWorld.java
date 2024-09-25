36937db87a0a0921213c464a51524924ea93db2e
adam23ahernandez <adam23ahernandez@inslaferreria.cat>
2024-09-25 16:36:19 +0200

    Àlex Hernández Gallego

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
+
 public class HelloWorld {
-  public static void main(String[] args){
-    System.out.println("Hello World 24-25\n----------------------------------");
-  }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World 24-25\n----------------------------------");
+        System.out.println("Àlex Hernández Gallego");
+    }
 }
