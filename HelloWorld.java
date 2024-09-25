1dbd06a1d4b1e11b6702a18bb086c94864944270
osorcom <oscar.sor.com@gmail.com>
2024-09-25 16:29:22 +0200

    osc

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args){
     System.out.println("Hello World 24-25\n----------------------------------");
+    System.out.println("osc");
   }
 }
